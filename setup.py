f9033e6c74f97e20df5d2357f93b1000ae935706
鼬君夏纪 <470766206@qq.com>
2025-04-01 11:19:10 +0800

    v0.0.3 新增setup.py文件以支持项目打包，更新.gitignore以忽略构建和分发相关文件。

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="gl-py2pyd",
+    version="0.1.1",
+    author="gu lei",
+    author_email="youjunxiaji@gmail.com",
+    description="将Python文件转换为pyd/so文件的工具",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/youjunxiaji/py2pyd-arg",
+    packages=["module"],
+    include_package_data=True,
+    py_modules=["py2pyd"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "cython",
+        "loguru",
+    ],
+    entry_points={
+        "console_scripts": [
+            "py2pyd=py2pyd:main",
+        ],
+    },
+) 
